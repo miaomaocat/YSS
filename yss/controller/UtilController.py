7eb4dab83d1bcf78c7348141d4bd183ca27c4327
root <root@worker-002.miaomaocat.com>
2014-06-19 14:20:24 +0000

    remove authorize for download

--- yss/controller/UtilController.py
+++ yss/controller/UtilController.py
@@ -9,18 +9,15 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 @app.route('/download/<chapterId>')
 def download(chapterId=None):
-    if not session.get('logged_in'):
-        abort(401)
-    else:
-        chapter = Chapter.chapterWithId(chapterId)
-        contentId = str(chapter.contentId)
-        chapterId = str(chapter.chapterId)
-        chapterFileName = chapter.chapterFileName
+    chapter = Chapter.chapterWithId(chapterId)
+    contentId = str(chapter.contentId)
+    chapterId = str(chapter.chapterId)
+    chapterFileName = chapter.chapterFileName
 
-        dir = os.path.join('Uploads', contentId)
-        dir = os.path.join(dir, chapterId)
-        dir = os.path.join(dir, chapterFileName)
-        return redirect(url_for('static', filename=dir))
+    dir = os.path.join('Uploads', contentId)
+    dir = os.path.join(dir, chapterId)
+    dir = os.path.join(dir, chapterFileName)
+    return redirect(url_for('static', filename=dir))
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
